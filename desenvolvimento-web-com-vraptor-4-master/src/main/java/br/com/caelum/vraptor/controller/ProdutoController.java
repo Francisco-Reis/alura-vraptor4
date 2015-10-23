89d67a953f0b5eb4e76b91739b3676b549c9c57e
Francisco F. Reis <franciscofreis@gmail.com>
2015-10-23 17:29:59 -0200

    Aula 3

--- desenvolvimento-web-com-vraptor-4-master/src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
+++ desenvolvimento-web-com-vraptor-4-master/src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.dao.ProdutoDao;
 import br.com.caelum.vraptor.model.Produto;
 import br.com.caelum.vraptor.util.JPAUtil;
@@ -23,5 +24,27 @@ public class ProdutoController {
 		EntityManager em = JPAUtil.criaEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		return produtoDao.lista();
+	}
+	
+	public void formulario(){
+		
+	}
+	
+	@Path("/produto/adiciona")
+	public void adiciona(Produto produto) {
+	    EntityManager em = JPAUtil.criaEntityManager();
+	    ProdutoDao produtoDao = new ProdutoDao(em);
+	    em.getTransaction().begin();
+	    produtoDao.adiciona(produto);
+	    em.getTransaction().commit();
+	}
+	
+	@Path("/produto/remove")
+	public void remove(Produto produto){
+	    EntityManager em = JPAUtil.criaEntityManager();
+	    ProdutoDao produtoDao = new ProdutoDao(em);
+	    em.getTransaction().begin();
+	    produtoDao.remove(produto);
+	    em.getTransaction().commit();
 	}
 }
---
+++ desenvolvimento-web-com-vraptor-4-master/src/main/webapp/WEB-INF/jsp/produto/formulario.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../base.css">
+<title>Inserir novo produto</title>
+</head>
+<body>
+
+
+<form action="<c:url value='/produto/adiciona'/>">
+    Nome:
+    <input class="form-control" type="text" name="produto.nome" value="${produto.nome}"/>
+    Valor:
+    <input class="form-control" type="text" name="produto.valor" value="${produto.valor}"/>
+    Quantidade:
+    <input class="form-control" type="text" name="produto.quantidade" value="${produto.quantidade}"/>
+    <button class="btn btn-primary" type="submit">Adicionar</button>
+    </form>
+
+
+</body>
+</html>
